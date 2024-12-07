7e4bef7735968d216c2ca20cff326e082275f1e2
alexdudley95 <alexander.dudley95@gmail.com>
2024-12-07 09:32:42 +0000

    solved d1 p2

--- day1/main.py
+++ day1/main.py
@@ -1,5 +1,6 @@
 totalDistance = 0
 remainder = 0
+simScore = 0
 leftList = list()
 rightList = list()
 for line in open('aocd2024/day1/input.txt'):
@@ -12,13 +13,22 @@ rightList.sort()
 
 for i in range(len(leftList)):
   if leftList[i] < rightList[i]:
-    print(leftList[i])
+    #print(leftList[i])
     remainder = rightList[i] - leftList[i]
     totalDistance += remainder
   elif rightList[i] < leftList[i]:
-    print("RIGHT")
+    #print("RIGHT")
     remainder = leftList[i] - rightList[i]
     totalDistance += remainder
 
+  compare = 0
+  for j in range(len(rightList)):
+    if leftList[i] == rightList[j]:
+      compare += 1
 
-print(totalDistance)
+  if compare > 0:
+    simScore += (compare * leftList[i])
+    compare = 0
+
+#print(totalDistance)
+print(simScore)
